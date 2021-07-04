daefd2e4b21a25632051ccf6fe4d2f21770967ce
Ronny Freites <ronnyangelo.freites@gmail.com>
2021-07-03 23:12:09 -0600

    feat: add cloud-app

---
+++ packages/cloud-app/bin/cloud-app.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import { CloudAppStack } from '../lib/cloud-app-stack';
+
+const app = new cdk.App();
+new CloudAppStack(app, 'CloudAppStack');
