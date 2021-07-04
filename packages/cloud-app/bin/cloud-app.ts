da1c5d608fb4017005477f9d3f27add92d18937d
Ronny Freites <ronnyangelo.freites@gmail.com>
2021-07-04 10:18:38 -0600

    feat: add yarn-workspaces dep

--- packages/cloud-app/bin/cloud-app.ts
+++ packages/cloud-app/bin/cloud-app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { CloudAppStack } from '../lib/cloud-app-stack';
+import * as cdk from "@aws-cdk/core";
+import { CloudAppStack } from "../lib/cloud-app-stack";
 
 const app = new cdk.App();
-new CloudAppStack(app, 'CloudAppStack');
+new CloudAppStack(app, "CloudAppStack");
