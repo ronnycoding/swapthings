eecffe25cba2df302e2c407cf22dc872d55e5156
Ronny Freites <ronnyangelo.freites@gmail.com>
2021-07-24 20:45:21 -0600

    chore(CloudApp): add cognito pool

--- packages/cloud-app/lib/cloud-app-stack.ts
+++ packages/cloud-app/lib/cloud-app-stack.ts
@@ -1,18 +1,67 @@
-import * as sns from "@aws-cdk/aws-sns";
-import * as subs from "@aws-cdk/aws-sns-subscriptions";
-import * as sqs from "@aws-cdk/aws-sqs";
 import * as cdk from "@aws-cdk/core";
+import * as cognito from "@aws-cdk/aws-cognito";
+
+const stackName = "SwapThings";
 
 export class CloudAppStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, "CloudAppQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
+    // The code that defines your stack goes here
+
+    // Add the cognito identity pool
+    const userPool = new cognito.UserPool(this, "userPool", {
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      emailSettings: {
+        // from: "noreply@ronnyiscoding.io",
+        replyTo: "noreply@ronnyiscoding.io",
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireUppercase: true,
+        requireLowercase: true,
+      },
+      selfSignUpEnabled: true,
+      signInCaseSensitive: true,
+      userPoolName: `${stackName}-user-pool`,
+      userVerification: {
+        emailBody:
+          "{username}, your account has been created. Please verify it by clicking the link below. {##Verify Email##}",
+        emailStyle: cognito.VerificationEmailStyle.LINK,
+        emailSubject: "Verify your account",
+      },
     });
 
-    const topic = new sns.Topic(this, "CloudAppTopic");
+    // Add the user pool client
+    const userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool,
+      accessTokenValidity: cdk.Duration.hours(1),
+      authFlows: {
+        userPassword: true,
+      },
+      refreshTokenValidity: cdk.Duration.days(1),
+      userPoolClientName: `${stackName}-user-pool-client`,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const identityPool = new cognito.CfnIdentityPool(this, "IdentityPool", {
+      allowUnauthenticatedIdentities: false, // Don't allow unathenticated users
+      cognitoIdentityProviders: [
+        {
+          clientId: userPoolClient.userPoolClientId,
+          providerName: userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    // Export values
+    new cdk.CfnOutput(this, "UserPoolId", {
+      value: userPool.userPoolId,
+    });
+    new cdk.CfnOutput(this, "UserPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+    new cdk.CfnOutput(this, "IdentityPoolId", {
+      value: identityPool.ref,
+    });
   }
 }
--- packages/cloud-app/test/cloud-app.test.ts
+++ packages/cloud-app/test/cloud-app.test.ts
@@ -1,23 +1,13 @@
-import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
-import * as cdk from "@aws-cdk/core";
-import * as CloudApp from "../lib/cloud-app-stack";
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as CloudApp from '../lib/cloud-app-stack';
 
-test("SQS Queue Created", () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new CloudApp.CloudAppStack(app, "MyTestStack");
-  // THEN
-  expectCDK(stack).to(
-    haveResource("AWS::SQS::Queue", {
-      VisibilityTimeout: 300,
-    })
-  );
-});
-
-test("SNS Topic Created", () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new CloudApp.CloudAppStack(app, "MyTestStack");
-  // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CloudApp.CloudAppStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
 });
