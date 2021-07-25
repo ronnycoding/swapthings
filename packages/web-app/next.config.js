b9908c49ed3a4d528cfda91da6e704eb037043cd
Ronny Freites <ronnyangelo.freites@gmail.com>
2021-07-24 22:33:28 -0600

    chore(WebApp): add amplify

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -1,3 +1,43 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    customKey: 'my-value',
+    aws: {
+      Auth: {
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: 'us-east-1:7a9e6baf-fac7-4e6a-b1cf-948d4d53f004',
+
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-east-1',
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-east-1_YoRcteZei',
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: '2dc2q8d6eek0ggdghmrdnk9v89',
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: false,
+
+        // OPTIONAL - Configuration for cookie storage
+        // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+        // cookieStorage: {
+        //   // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+        //   domain: '.yourdomain.com',
+        //   // OPTIONAL - Cookie path
+        //   path: '/',
+        //   // OPTIONAL - Cookie expiration in days
+        //   expires: 365,
+        //   // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+        //   sameSite: 'strict',
+        //   // OPTIONAL - Cookie secure flag
+        //   // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+        //   secure: true,
+        // },
+
+        // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+        authenticationFlowType: 'USER_PASSWORD_AUTH',
+      },
+    },
+  },
 }
--- packages/web-app/pages/_app.tsx
+++ packages/web-app/pages/_app.tsx
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
+// import { AmplifyAuthenticator } from '@aws-amplify/ui-react'
 import type { AppProps } from 'next/app'
+import { Amplify } from 'aws-amplify'
+const AmplifyConfig = process.env.aws || {}
+
+Amplify.configure({ ...AmplifyConfig, ssr: true })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
